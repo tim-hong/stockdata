1d81f7d64d5617cda688ba93e1d71c99dd64838d
tim-hong <timothy.d.hong@gmail.com>
2019-04-21 00:03:42 -0700

    Updates

---
+++ precache-manifest.f6bb9bcac4ad434ebac0c46c06701ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d6829b3488cfa6ae874",
+    "url": "/stockdata/static/js/runtime~main.78d5c8bb.js"
+  },
+  {
+    "revision": "5b657a1a854a773418f6",
+    "url": "/stockdata/static/js/main.92d51cf8.chunk.js"
+  },
+  {
+    "revision": "ac0a223f9ce380c87720",
+    "url": "/stockdata/static/js/2.df5f9a4c.chunk.js"
+  },
+  {
+    "revision": "5b657a1a854a773418f6",
+    "url": "/stockdata/static/css/main.1981180a.chunk.css"
+  },
+  {
+    "revision": "ac0a223f9ce380c87720",
+    "url": "/stockdata/static/css/2.47f36d31.chunk.css"
+  },
+  {
+    "revision": "e74543c29e48ac2eb952dbcf4b43f9e7",
+    "url": "/stockdata/index.html"
+  }
+];
